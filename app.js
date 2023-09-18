ffb7d87037f7238f8f683a4ab41d86873517154a
Jayesh <jayeshrpandav@gmail.com>
2023-09-18 19:59:38 +0530

    server is now listening on PORT or 5000

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 const express = require("express");
+const Joi = require("joi");
+const userSchema = require("./models/userModel");
+const connectDatabase = require("./config/db");
+
 const app = express();
-const cors = require("cors");
-app.use(cors());
+app.use(express.json());
+
+app.post("/register", async (req, res) => {
+  try {
+    const { error, value } = userSchema.validate(req.body);
+
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
+
+    const User = require("./models/userModel");
+    const newUser = new User(value);
+    await newUser.save();
+
+    res
+      .status(201)
+      .json({ message: "User data saved successfully", user: newUser });
+  } catch (error) {
+    console.error("Error saving user data:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 module.exports = app;
--- index.js
+++ index.js
@@ -17,8 +17,8 @@ dotenv.config({ path: "config/.env" });
 // connecting database
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
-  console.log(`server is listening`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`server is listening on port ${process.env.PORT || 5000}`);
 });
 
 // Unhandled Promise Rejection
