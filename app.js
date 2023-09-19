aebe9d360561bc6fc1b442a87eb2192234f11ae3
Jayesh <jayeshrpandav@gmail.com>
2023-09-20 02:31:37 +0530

    added /getmembers route

--- app.js
+++ app.js
@@ -33,4 +33,14 @@ app.post("/register", async (req, res) => {
   }
 });
 
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 module.exports = app;
